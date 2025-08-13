ac2eec417f7798fff169107923f2775f665e2708
Kathleen Branche <kathleenbranche@yahoo.com>
2025-08-12 22:23:54 -0400

    Fix test

--- http-server.test.js
+++ http-server.test.js
@@ -5,6 +5,6 @@ describe('Hello World Server', () => {
   test('GET / returns Hello World response', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('This will fail');
+    expect(response.text).toBe('Hello World');
   });
 });
